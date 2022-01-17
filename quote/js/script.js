738d6b90539862c72efcf78da5e8aa21ffc7acc3
suhailauitm15 <97877681+suhailauitm15@users.noreply.github.com>
2022-01-17 19:33:12 +0800

    Summary

--- quote/js/script.js
+++ quote/js/script.js
@@ -1,4 +1,28 @@
 const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("button"),
 authorName = document.querySelector(".name"),
 speechBtn = document.querySelector(".speech"),
 synth = speechSynthesis;
+
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "Want More!";
+    });
+}
+
+speechBtn.addEventListener("click", ()=>{
+    if(!quoteBtn.classList.contains("loading")){
+        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+        synth.speak(utterance);
+        setInterval(()=>{
+            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
+        }, 10);
+    }
+});
+
+quoteBtn.addEventListener("click", randomQuote);
